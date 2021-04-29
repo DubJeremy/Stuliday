257bd0c6c504e71303eeabeb3c473618e100eebf
DubJ <dubois.jeremy33@gmail.com>
2021-04-29 15:16:51 +0200

    page profil

--- profil.php
+++ profil.php
@@ -1,5 +1,19 @@
 <?php require 'nav.php' ?>
     <body>
+        <section id="profil">
+            <h2>
+                Bonjour user!
+            </h2>
+            <h3>Vos annonces:</h3>
+            <div class="aptsheet">
+                <img src="assets/images/apt.jpg" alt="apartment">
+                <h3>Appartement</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quam voluptas voluptatum fuga laudantium corporis laboriosam sint, id dolor eos, ab voluptate aliquam. Accusamus ab sed dolore quasi voluptates doloremque!</p>
+                <a href="fileproduct.php">annonce</a>               
+            </div>
+            <a href="newproduct.php">Ajouter une annonce</a>
+
+        </section>
 
         <script src="assets/lib/jquery-3.6.0.min.js"></script>
         <script src="assets/js/script.js"></script>  
